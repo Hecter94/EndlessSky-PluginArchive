87d3c95d29d6d3fa5c420190fb0c1aee75f3cc9d
zuckung <zuckung@gmx.de>
2023-07-06 21:06:37 +0200

    improved updating mechanism (#82)
    * added 131 direct update links
    
    * modified checkdirectlink.py to handle github generated zips from single plugin repos
    
    * changed template.txt with new informations for updating
    
    * rewrote to work with github api
    
    * updated template.txt with more plugin file informations
    
    * added author tag to news
    
    * added author tag to news
    
    * added author tag to news
    
    * Update additional.command.buttons.txt
    
    * Update res/template.txt
    
    Co-authored-by: Hecter94 <Hecter94@gmail.com>
    
    * Update res/template.txt
    
    Co-authored-by: Hecter94 <Hecter94@gmail.com>
    
    * Update res/template.txt
    
    Co-authored-by: Hecter94 <Hecter94@gmail.com>
    
    * Update res/template.txt
    
    Co-authored-by: Hecter94 <Hecter94@gmail.com>
    
    ---------
    
    Co-authored-by: Hecter94 <Hecter94@gmail.com>

--- res/src/checkdirectlinks.py
+++ res/src/checkdirectlinks.py
@@ -28,81 +28,109 @@ with open("res/config.txt") as f:
 			pluginurl = line.split(" = ")[1]
 		if line.find("assetfullpath") == 0:	# i.e. assetfullpath = https://github.com/zuckung/test3/releases/download/Latest/
 			assetfullpath = line.split(" = ")[1]
+
 			
-			
-			
-			
+if os.path.isdir("temp") == False: # creating temp directory
+	os.mkdir("temp")						
+
+
+username = os.getenv("GITHUB_ACTOR")
+token = os.getenv("github_token")
+
+
+
 entries = os.listdir(listfolder) # for all listfiles
-entries.sort(key=str.lower)
+entries.sort(key=str.lower)		
 for entry in entries:
 	with open(listfolder + entry, "r") as file1:
 		x = file1.readline()
 		x = file1.readline()
 		x = file1.readline()
 		directlink = x.replace("directlink=","")
-		if directlink != "N/A\n": # when there is a direct link
-			directlink = directlink.strip()
-			pluginname = entry[:len(entry) -4]
-			print("\ndirectlink found for: " + pluginname)
-			withdots = pluginname.replace(" ", ".") 
-			withdots = withdots.replace("'", ".")
-			withdots = withdots.replace(",", ".") 
-			withdots = withdots.replace("(", ".") 
-			withdots = withdots.replace(")", ".") 
-			withdots = withdots.replace("&", ".")  
-			withdots = withdots.replace("...", ".")
-			withdots = withdots.replace("..", ".")
-			if withdots[len(withdots)-1] == ".":
-				withdots = withdots[:len(withdots)-1]
-			directzip = directlink.split(os.sep)
-			directzip = str(directzip[len(directzip)-1])
-			if directzip[:len(directzip) - 4] != pluginname: # checking for same name
-				print("ABORTING: naming convention mismatch")
+	if directlink == "N/A\n": # when there is a direct link
+		continue
+	else:
+		directlink = directlink.strip()
+		pluginname = entry[:len(entry) -4] # removes '.txt'
+		print("\ndirectlink found for: " + pluginname)
+		withdots = pluginname.replace(" ", ".") # getting asset file name
+		withdots = withdots.replace("'", ".")
+		withdots = withdots.replace(",", ".") 
+		withdots = withdots.replace("(", ".") 
+		withdots = withdots.replace(")", ".") 
+		withdots = withdots.replace("&", ".")  
+		withdots = withdots.replace("...", ".")
+		withdots = withdots.replace("..", ".")
+		if withdots[len(withdots)-1] == ".":
+			withdots = withdots[:len(withdots)-1]
+		directzip = directlink.split(os.sep)
+		directzip = str(directzip[len(directzip)-1])
+		
+		try: # check if asset file is there, and get assetlastmodified
+			response = requests.head(assetfullpath + withdots + ".zip", allow_redirects=True)
+			response.raise_for_status()
+		except requests.exceptions.HTTPError as err:
+			print(err)
+			assetlastmodified = "FALSE"
+		else:
+			modif = response.headers['Last-Modified']
+			datetime_object = datetime.strptime(modif, '%a, %d %b %Y %H:%M:%S %Z')
+			assetlastmodified = datetime_object.date()
+		
+		try: # check directlink
+			response = requests.head(directlink, allow_redirects=True)
+			response.raise_for_status()
+		except requests.exceptions.HTTPError as err:
+			print("ABORTING: directlink not reachable")
+			print(err)
+			continue
+		else: 
+			try: # checking if last-modified/content-length tags are there
+				modif = response.headers['Last-Modified']
+				datetime_object = datetime.strptime(modif, '%a, %d %b %Y %H:%M:%S %Z')
+				linklastmodified = datetime_object.date()
+				linksize = int(response.headers['Content-Length']) / 1024 # get size in kb
+			except:
+				if directlink[:18] == "https://github.com": # check if github repo
+					urllist = directlink.split(os.sep)
+					author = urllist[3]
+					plug = urllist[4]
+					linksize = "FALSE"
+					try: # check gifhub api for last commit
+						params = {'page': '1', 'per_page': '1'}
+						response = requests.get('https://api.github.com/repos/' + author +'/' + plug + '/commits', params=params, auth=(username,token))
+					except:
+						print("ABORTING: github api not reachable")
+						linklastmodified = "FALSE"
+						continue
+					else: # get last commit date and time
+						cont = str(response.content)
+						dateandtime = cont.split(",")[4]
+						commitdate = dateandtime[8:18] 
+						committime = dateandtime[19:27]
+						linklastmodified = datetime.strptime(commitdate, '%Y-%m-%d').date()
+		if linksize != "FALSE":
+			if linksize >= 102400:
+				print("ABORTING: directlink is bigger than 100 mb")
+				continue
+		if linklastmodified != "FALSE":
+			if assetlastmodified != "FALSE":
+				datediff = linklastmodified - assetlastmodified # both lastmodified were successful, compare them
+				if datediff.days < 1: 
+					print("ABORTING: assetfile is newer | link: " + str(linklastmodified) + " asset: " + str(assetlastmodified) + " datediff: " + str(datediff.days))
+				else:	
+					print("SUCCESS: linkfile is newer")
+					with open("temp/" + pluginname + ".zip", "wb") as file2: # create zip file
+						r = requests.get(directlink, allow_redirects=True)
+						file2.write(r.content)
+						print("SUCCESS: downloaded zip")
 			else:
-				print("SUCCESS: naming convention match")		
-						
-				try: # check asset file
-					response = requests.head(assetfullpath + withdots + ".zip", allow_redirects=True)
-					response.raise_for_status()
-				except requests.exceptions.HTTPError as err:
-					print(err)
-				else:
-					response = requests.head(assetfullpath + withdots + ".zip", allow_redirects=True)
-					response.raise_for_status()
-					modif = response.headers['Last-Modified']
-					datetime_object = datetime.strptime(modif, '%a, %d %b %Y %H:%M:%S %Z')
-					assetlastmodified = datetime_object.date()
-					try: # check directlink
-						response = requests.head(directlink, allow_redirects=True)
-						response.raise_for_status()
-					except requests.exceptions.HTTPError as err:
-						print(err)
-					else:
-						response = requests.head(directlink, allow_redirects=True)
-						response.raise_for_status()
-						modif = response.headers['Last-Modified']
-						datetime_object = datetime.strptime(modif, '%a, %d %b %Y %H:%M:%S %Z')
-						linklastmodified = datetime_object.date()
-						datediff = linklastmodified - assetlastmodified # both lastmodified were successful, compare them
-						if datediff.days < 1: 
-							print("ABORTING: assetfile is newer | link: " + str(linklastmodified) + " asset: " + str(assetlastmodified) + " datediff: " + str(datediff.days))
-						else:	
-							print("SUCCESS: linkfile is newer")
-						
-							assetsize = int(response.headers['Content-Length']) / 1024 # get size in kb
-							if assetsize >= 102400:
-								print(assetsize)
-								print("ABORTING: directlink is bigger than 100 mb")
-							else:
-								print("SUCCESS: file is smaller than 100 mb")
-								if os.path.isdir("temp") == False:
-									os.mkdir("temp")
-								with open("temp/" + directzip, 'wb') as file2:
-									r = requests.get(directlink, allow_redirects=True)
-									file2.write(r.content)
-									print("SUCCESS: downloaded zip")
-						
-
+				print("no assetfile, must be a new plugin")
+				with open("temp/" + pluginname + ".zip", "wb") as file2: # create zip file
+					r = requests.get(directlink, allow_redirects=True)
+					file2.write(r.content)
+					print("SUCCESS: downloaded zip")
+				
 # extracting zips
 listing = glob.glob("temp/*.zip")
 print("\nlast modified checks DONE, extracting zips now")
@@ -119,7 +147,7 @@ for entry in listing:
 		# check for same names of zip and first folder in zip
 		if stripped != firstfolder:
 			print("ERROR: mismatch between zipname and in-zip folder!")
-			shutil.move(firstfolder, stripped)
+			shutil.move("temp/" + firstfolder, "temp/" + stripped)
 			print("temp/" + firstfolder + " | renamed to: temp/" + stripped)
 		if os.path.isdir(pathtoplugins + stripped):
 			shutil.rmtree(pathtoplugins + stripped) # delete old plugin
@@ -129,7 +157,9 @@ for entry in listing:
 	with open("res/news.txt", "w") as file1: # write to news file, newest on top, keep old contents
 		today = datetime.today().strftime('%Y-%m-%d')
 		with open("res/pluginlist/" + stripped + ".txt") as file2:
-			c = file2.readline()
+			author = file2.readline()
+			author = author.replace("author=", "")
+			author = author.strip()
 			c = file2.readline()
 			c = file2.readline()
 			c = file2.readline()
@@ -138,7 +168,7 @@ for entry in listing:
 			if c == "N/A":
 				c = "uncategorized"
 			c = "[" + c + "](" + webroot + indexfile + "#" + c + ")\n" # anchor link to category
-		news = [today + " '" + stripped + "' updated | category: " + c] + news
+		news = [today + " '" + stripped + "' updated by " + author + " | category: " + c] + news
 		file1.writelines(news)
 	# renaming zips to asset convention
 	withdots = stripped.replace(" ", ".") 
@@ -152,4 +182,3 @@ for entry in listing:
 	if withdots[len(withdots)-1] == ".":
 		withdots = withdots[:len(withdots)-1]
 	shutil.move(entry, "temp/" + withdots + ".zip")
-					
