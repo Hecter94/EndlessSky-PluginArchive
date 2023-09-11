bef443018d10fcdfecab8cc1add9bcf7a67b243a
script <>
2023-09-11 23:15:11 +0000

    Plugins updated

--- Working/Ruin the Fun/tools/install-supported-plugins.sh
+++ Working/Ruin the Fun/tools/install-supported-plugins.sh
@@ -3,16 +3,17 @@ declare -A plugins
 plugins[Aben]=https://github.com/Adde-Endless-Sky/Aben
 plugins[Adamas Project]=https://github.com/kestrel1110/Adamas-Project
 plugins[Amphibious Ships]=https://github.com/AvianGeneticist/Amphibious-Ships
-#plugins[Arion]=https://github.com/Spectre907YT/Endless-Sky---Arion
+plugins[Arion]=https://github.com/Spectre907YT/Endless-Sky---Arion
 plugins[Beyond the Sky]=https://github.com/1010todd/Beyond-the-Sky
-#plugins[Blended Ships]=https://github.com/mOctave/blended-ships
+plugins[Blended Ships]=https://github.com/mOctave/blended-ships
 plugins[BSRC]=https://github.com/phenix2/BSRC
 plugins[Celestial Strands]=https://github.com/Saugia/celestial-strands
-#plugins[Chroma Expansion]=https://github.com/OcelotWalrus/Cromha-Expansion-plugin
+plugins[Chroma Expansion]=https://github.com/OcelotWalrus/Cromha-Expansion-plugin
 #plugins[Czartraks Ship Pack]=https://github.com/czartrak/Czartraks-Ship-Pack
 	# error in data
 plugins[Derogam]=https://github.com/Lorantine/derogam
-#plugins[ES Galactic Exploration]=https://github.com/lumbar527/ES-Galactic-Exploration
+plugins[ES Galactic Exploration]=https://github.com/lumbar527/ES-Galactic-Exploration
+plugins[Eternals]=https://github.com/comnom/Eternals
 plugins[Fighter Factory]=https://github.com/1010todd/Fighter-Factory
 plugins[Fire Corporation Plugin]=https://github.com/lumbar527/Fire-Corporation
 plugins[Fluff]=https://github.com/Adde-Endless-Sky-Mods/Fluff
@@ -32,11 +33,11 @@ plugins[Midnight Expansion]=https://github.com/MidnightPlugins/Midnight-Expansio
 plugins[Midnight Scrapyard]=https://github.com/MidnightPlugins/Midnight-Scrapyard
 plugins[Mining Pinger]=https://github.com/AvianGeneticist/MiningPinger
 plugins[sensor]=https://github.com/orbitalsupershell/sensor/
-#plugins[Sestor Expansion]=https://github.com/AvianGeneticist/Sestor-Expansion
-#plugins[Sheragi Rebirth]=https://github.com/Petersupes/sheragi-rebirth
+plugins[Sestor Expansion]=https://github.com/AvianGeneticist/Sestor-Expansion
+plugins[Sheragi Rebirth]=https://github.com/Petersupes/sheragi-rebirth
 plugins[Spinaleviathan]=https://github.com/AvianGeneticist/Spinaleviathan
 plugins[The Enclave]=https://github.com/1010todd/The-Enclave
-#plugins[The Peacebringer]=https://github.com/Polaria1/The-Peacebringer
+plugins[The Peacebringer]=https://github.com/Polaria1/The-Peacebringer
 plugins[Unfettered Innovations]=https://github.com/Hurleveur/unfettered-innovations
 
 pushd ../../plugins > /dev/null
--- Working/Ruin the Fun/tools/ls-data-plugins.sh
+++ Working/Ruin the Fun/tools/ls-data-plugins.sh
@@ -1,2 +1,2 @@
 #!/bin/env sh
-ls -d -1 ../../plugins/*/data/ | sed 's|^''\(.*\)''$|\1|' | sed 's|^../../plugins/||' | sed 's|/data/$||' | sed '/^-/d' | sed '/ruin-the-fun/d' | sed '/lissas/d'
+ls -d -1 ../../plugins/*/data/ | sed 's|^''\(.*\)''$|\1|' | sed 's|^../../plugins/||' | sed 's|/data/$||' | sed '/^-/d' | sed '/ruin-the-fun/d' | sed '/Ruin[ -]The[ -]Fun/d' | sed '/lissas/d'
